18169b32b06c640a6e1f400b58daa818de0fbdb3
sunsound33 <sun.sound33@gmail.com>
2022-03-22 03:52:11 +0900

    navBar修正

--- next/src/components/Navbar/Navbar.tsx
+++ next/src/components/Navbar/Navbar.tsx
@@ -1,28 +1,83 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { useState } from "react";
+
+
+
 const Navbar:NextPage = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
   return (
     <>
       <nav className="bg-white shadow dark:bg-gray-800">
-        <ul className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
-          <ul className="flex items-center justify-between">
-            <li>
+        <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+          <div className="flex items-center justify-between">
+            <div>
               <Link href="/">
                   <a className="text-2xl font-bold text-gray-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300">EC-site</a>
               </Link>
-            </li>
+            </div>
 
-            <li className="flex md:hidden">
-              <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-                <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                  <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                </svg>
-              </button>
-            </li>
-          </ul>
+            <div className="flex md:hidden">
+              {isOpen ? (
+              <>
+                <button className="absolute top-0 right-3 px-4 py-4" onClick={() => setIsOpen(false)} >
+                  <svg
+                    className="h-8 w-8 text-white"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                      <line x1="18" y1="6" x2="6" y2="18" />
+                      <line x1="6" y1="6" x2="18" y2="18" />
+                    </svg>
+                </button>
+              </>
+              ) : (
+                <>
+                  <button onClick={() => setIsOpen(true)} type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                    <div className="space-y-2">
+                      <span className="block w-8 h-0.5 bg-white"></span>
+                      <span className="block w-8 h-0.5 bg-white"></span>
+                      <span className="block w-5 h-0.5 bg-white"></span>
+                    </div>
+                  </button>
+                </>) }
+            </div>
+          </div>
+          {isOpen ? (
+          <>
+            <div className="md:hidden">
+              <div className="flex flex-col">
+                <Link href="/">
+                  <a className="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0">Home</a>
+                </Link>
+                <Link href="/shop">
+                  <a className="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="/shop">Shop</a>
+                </Link>
+                <Link href="/contact">
+                  <a className="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="/contact">Contact</a>
+                </Link>
+                <Link href="/about">
+                  <a className="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="/about">About</a>
+                </Link>
+              </div>
+            </div>
+            <div className="flex md:block">
+                <a className="relative text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="/cart">
+                  <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  </svg>
+                  <span className="absolute top-0 left-0 p-1 text-xs text-white bg-blue-500 rounded-full"></span>
+                </a>
+              </div>
+          </>) : false}
 
-          <ul className="items-center md:flex">
-            <li className="flex flex-col md:flex-row md:mx-6">
+          <div className="items-center md:flex">
+            <div className="md:flex hidden">
               <Link href="/">
                 <a className="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0">Home</a>
               </Link>
@@ -35,18 +90,18 @@ const Navbar:NextPage = () => {
               <Link href="/about">
                 <a className="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="/about">About</a>
               </Link>
-            </li>
 
-            <li className="flex justify-center md:block">
-              <a className="relative text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="/cart">
-                <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                <span className="absolute top-0 left-0 p-1 text-xs text-white bg-blue-500 rounded-full"></span>
-              </a>
-            </li>
-          </ul>
-        </ul>
+              <div className="flex md:block">
+                <a className="relative text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="/cart">
+                  <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  </svg>
+                  <span className="absolute top-0 left-0 p-1 text-xs text-white bg-blue-500 rounded-full"></span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
     </>
   );
--- next/src/pages/about.tsx
+++ next/src/pages/about.tsx
@@ -4,7 +4,6 @@ const About = () => {
   return(
     <>
       <Header/>
-
       <h1 className="text-center ">About</h1>
     </>
   );
